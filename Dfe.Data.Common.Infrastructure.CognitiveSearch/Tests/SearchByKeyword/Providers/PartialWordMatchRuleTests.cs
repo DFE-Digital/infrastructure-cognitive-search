b2d947d75a29f19b785373d5a05999917d494f81
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-02-03 10:06:40 +0000

    remove search rule opts (#56)

--- Dfe.Data.Common.Infrastructure.CognitiveSearch/Tests/SearchByKeyword/Providers/PartialWordMatchRuleTests.cs
+++ Dfe.Data.Common.Infrastructure.CognitiveSearch/Tests/SearchByKeyword/Providers/PartialWordMatchRuleTests.cs
@@ -1,11 +1,10 @@
-﻿using Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByKeyword.Options;
-using Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByKeyword.SearchRules;
+﻿using Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByKeyword.SearchRules;
 using FluentAssertions;
 using Xunit;
 
 namespace Dfe.Data.Common.Infrastructure.CognitiveSearch.Tests.SearchByKeyword.Providers;
 
-public class PartialWordMatchRuleTests
+public sealed class PartialWordMatchRuleTests
 {
     [Theory]
     [InlineData("searchKeyword", "searchKeyword*")]
@@ -14,51 +13,14 @@ public class PartialWordMatchRuleTests
     void ApplySearchRules_WithPartialWordMatchRuleOption_AppliesRule(string searchInput, string expected)
     {
         // arrange
-        var searchRulesOptions = new SearchRuleOptions()
-        {
-            SearchRule = "PartialWordMatch"
-        };
 
-        var provider = new CognitiveSearch.SearchByKeyword.SearchRules.PartialWordMatchRule(searchRulesOptions);
+        PartialWordMatchRule provider = new();
 
         // act
-        var searchKeywordResult = provider.ApplySearchRules(searchInput);
+        string searchKeywordResult = provider.ApplySearchRules(searchInput);
 
         // assert
         searchKeywordResult.Should().Be(expected);
         searchInput.Should().NotBe(expected);
-    }
-
-    [Fact]
-    void ApplySearchRules_WithNotApplicableRuleOption_ReturnsUnmodified()
-    {
-        // arrange
-        var searchRulesOptions = new SearchRuleOptions()
-        {
-            SearchRule = "a nonexistent rule"
-        };
-
-        var provider = new PartialWordMatchRule(searchRulesOptions);
-
-        // act
-        var searchKeywordResult = provider.ApplySearchRules("searchKeyword");
-
-        // assert
-        searchKeywordResult.Should().Be("searchKeyword");
-    }
-
-    [Fact]
-    void ApplySearchRules_WithNoOption_ReturnsUnmodified()
-    {
-        // arrange
-        var searchRulesOptions = new SearchRuleOptions();
-
-        var provider = new PartialWordMatchRule(searchRulesOptions);
-
-        // act
-        var searchKeywordResult = provider.ApplySearchRules("searchKeyword");
-
-        // assert
-        searchKeywordResult.Should().Be("searchKeyword");
     }
 }
